ec6cbdc7fbbca0a8a89a645bc1ff7e06fcec842e
James Pope <james_pope@student.uml.edu>
2022-12-26 18:55:53 -0500

    changed from packer to lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,18 @@
-local vim = vim
+-- Bootstrapping lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.runtimepath:prepend(lazypath)
 
-require("plugins")
+-- Packages for nvim
+require("lazy").setup({
+        'Yazeed1s/minimal.nvim',
+})
