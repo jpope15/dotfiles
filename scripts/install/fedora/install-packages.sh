a226cc05ec28f2f83b21faa81089563b96a4c637
James Pope <james_pope@student.uml.edu>
2023-01-22 20:51:47 -0500

    removed old scripts directory

--- scripts/install/fedora/install-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This will install all packages in the packages.txt file
-config_file=~/.config/install-deps/fedora-install-packages.txt
-
-while read -r line; do
-    sudo dnf install -y $line
-done < $config_file 
