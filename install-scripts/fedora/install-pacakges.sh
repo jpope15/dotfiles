9c8cf008c4145fac443e6215cce19563ef8c2950
James Pope <james_pope@student.uml.edu>
2023-01-22 19:42:38 -0500

    added install-deps to the .config directory

--- install-scripts/fedora/install-pacakges.sh
+++ install-scripts/fedora/install-pacakges.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# This will install all pacakges in the packages.txt file
-config_file='fedora-install-pacakges.txt'
+# This will install all packages in the packages.txt file
+config_file=~/.config/install-deps/fedora-install-packages.txt
 
 while read -r line; do
     sudo dnf install -y $line
