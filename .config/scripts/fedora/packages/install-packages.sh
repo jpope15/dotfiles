6f7c28a0ec03834a3988fcbbf6217471b2f2b61a
James Pope <james_pope@student.uml.edu>
2023-01-23 20:01:19 -0500

    fixed config file path

--- .config/scripts/fedora/packages/install-packages.sh
+++ .config/scripts/fedora/packages/install-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This will install all packages in the packages.txt file
-config_file=~/.config/install-deps/fedora-install-packages.txt
+config_file=~/.config/scripts/fedora/packages/fedora-install-packages.txt
 
 while read -r line; do
     sudo dnf install -y $line
