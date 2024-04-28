8ba7cb81e56d01f50d543fb63b1d386a09b1e9f7
James Pope <james_pope@student.uml.edu>
2024-04-28 18:48:39 -0400

    updated nvim config

---
+++ .config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,7 @@
+---@type ChadrcConfig
+local M = {}
+
+M.mappings = require "custom.mappings"
+M.ui = { theme = 'onedark' }
+
+return M
---
+++ .config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+-- Your custom mappings
+M.abc = {
+  i = {
+     ["jk"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }}
+  }
+}
+
+return M
